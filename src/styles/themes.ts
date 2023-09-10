21127759e8332ed49a0bbaf794ccf5f77132526f
matheusjean <matheusjean11@hotmail.com>
2023-09-10 17:03:50 -0300

    feat: create theme styles

---
+++ src/styles/themes.ts
@@ -0,0 +1,35 @@
+'use client'
+export default {
+  grid: {
+    container: '91vh',
+    mainWidth: '100%',
+  },
+  border: {
+    radius: {
+      medium: '15px',
+    },
+  },
+  font: {
+    sizes: {
+      xxxxsmall: '0.6rem',
+      xxxsmall: '0.7rem',
+      xxsmall: '1.1rem',
+      xsmall: '1.5rem',
+      small: '0.8rem',
+    },
+  },
+  colors: {
+    primary: '#e0e0e0',
+    gray: 'rgba(255, 255, 255, 0.4)',
+  },
+  spacings: {
+    xsmall: '10px',
+    xxsmall: '15px',
+    small: '20px',
+  },
+  screen: {
+    medium: '768px',
+    large: '1200px',
+    big: '1720px',
+  },
+} as const
