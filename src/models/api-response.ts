ecf67141e49710994d937481262ed27d7bd98cd7
matheusjean <matheusjean11@hotmail.com>
2023-09-10 17:06:26 -0300

    feat: create interface model for response

---
+++ src/models/api-response.ts
@@ -0,0 +1,13 @@
+export type ApiResponse = {
+  uuid: string
+  zipcode: string
+  state: string
+  city: string
+  streetName: string
+  description: string
+  neighbourhood: string
+  streetNumber: number
+  latitude: number
+  longitude: number
+  createdAt: Date
+}
